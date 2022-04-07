b509a35bc5a5e403f42a4e4c6a04979313704ed3
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-04-07 12:27:18 +0300

    Initial commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public string TranslatePostfixNotation(string expression)
+        {
+            string result = "";
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (IsDelimiter(expression[i]))
+                {
+                    continue;
+                }
+                else if (Char.IsDigit(expression[i]))
+                {
+                    while (!IsDelimiter(expression[i]))
+                    {
+                        result += expression[i];
+                        i++;
+                    }
+                }
+                else
+                {
+
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsDelimiter(char simbol)
+        {
+            bool isDelimiter = false;
+
+            if (simbol == ' ')
+            {
+                isDelimiter = true;
+            }
+
+            return isDelimiter;
+        }
+
+        private bool IsOperator(char simbol)
+        {
+            bool isOperator = false;
+
+            if ("*/+-()".IndexOf(simbol) != -1)
+            {
+                isOperator = true;
+            }
+
+            return isOperator;
+        }
+
+    }
+}
